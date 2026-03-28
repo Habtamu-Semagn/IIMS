1de46ac0325e378379319b398def2784f043ccde
bitusmaximus <btesfa500@gmail.com>
2026-03-28 14:11:05 +0300

    src folder removed

--- backend/app.module.ts
+++ backend/app.module.ts
@@ -3,12 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
-import { AuthModule } from 'auth/auth.module';
-import { SchoolModule } from 'school/school.module';
-import { AssetModule } from 'asset/asset.module';
-import { TicketModule } from 'ticket/ticket.module';
-import { CurriculumModule } from 'curriculum/curriculum.module';
-import { TrainingModule } from 'training/training.module';
+import { AuthModule } from './auth/auth.module';
+import { SchoolModule } from './school/school.module';
+import { AssetModule } from './asset/asset.module';
+import { TicketModule } from './ticket/ticket.module';
+import { CurriculumModule } from './curriculum/curriculum.module';
+import { TrainingModule } from './training/training.module';
 
 @Module({
   imports: [
--- backend/test/setup.ts
+++ backend/test/setup.ts
@@ -1,6 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../app.module';
+import { AppModule } from '../../app.module';
 import { PrismaService } from '../prisma/prisma.service';
 
 export async function createTestApp(): Promise<{
