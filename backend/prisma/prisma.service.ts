84a1537f6335caabd8a889c829e92c59979d6e20
bitusmaximus <btesfa500@gmail.com>
2026-03-28 23:47:42 +0300

    merge for config files

--- backend/prisma/prisma.service.ts
+++ backend/prisma/prisma.service.ts
@@ -7,24 +7,12 @@ import { Pool } from 'pg';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-<<<<<<< HEAD
-    // Standard Node-Postgres Pool
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-    // Wrap it in the PrismaPg adapter
-    const adapter = new PrismaPg(pool);
-
-    super({ adapter });
-  }
-
-=======
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
     const adapter = new PrismaPg(pool);
 
     // The super call passes the adapter to the core engine
     super({ adapter });
   }
->>>>>>> feature/bitusmaximus
   async onModuleInit() {
     await this.$connect(); // This will stop throwing an error once 'generate' works
   }
