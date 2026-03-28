80d3e6d43d37375c34c2da5594cc45a992dc2d04
bitusmaximus <btesfa500@gmail.com>
2026-03-28 06:07:02 +0300

    config files for current implementations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { AuthModule } from 'auth/auth.module';
+import { SchoolModule } from 'school/school.module';
+import { AssetModule } from 'asset/asset.module';
+import { TicketModule } from 'ticket/ticket.module';
+import { CurriculumModule } from 'curriculum/curriculum.module';
+import { TrainingModule } from 'training/training.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
+    SchoolModule,
+    AssetModule,
+    TicketModule,
+    CurriculumModule,
+    TrainingModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
